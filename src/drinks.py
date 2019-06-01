395aa26448ba6c9b2d994aa693f6e74db4d1f254
Daniel Kiedrowski <daniel@levire.com>
2019-06-01 15:24:08 +0200

    ADDED recipies for malte

--- src/drinks.py
+++ src/drinks.py
@@ -1,113 +1,281 @@
 # drinks.py
 
-drink_list = [
-    {
-        "name": "...under\nconstruction",
-        "recipe": [
-        ]
-    }, {
-        "name": "Virgin Sunrise",
-        "recipe": [
-            ("oj", 160),
-            ("gren", 8)
-        ]
-    }, {
-        "name": "Exotic",
-        "recipe": [
-            ("oj", 90),
-            ("pineapple", 60),
-            ("mango", 30),
-            ("curacao", 16)
-            #("cocos", 16)
-        ]
-    }, {
-        "name": "PineMate",
-        "recipe": [
-            ("mate", 100),
-            ("pineapple", 75),
-            ("curacao", 16)
-            #("cocos", 20)
-        ]
-    }, {
-        "name": "Happy",
-        "recipe": [
-            ("mate", 100),
-            ("mango", 50),
-            ("lemonjuice", 20)
-        ]
-    }, {
-        "name": "...under\nconstruction",
-        "recipe": [
-        ]
-    }, {
-        "name": "...under\nconstruction",
-        "recipe": [
-        ]
-    }, {
-        "name": "...under\nconstruction",
-        "recipe": [
-        ]
-    }, {
-        "name": "...under\nconstruction",
-        "recipe": [
-        ]
-    }, {
-        "name": "...under\nconstruction",
-        "recipe": [
-        ]
-    }, {
-        "name": "...under\nconstruction",
-        "recipe": [
-        ]
-    }, {
-        "name": "Bo",
-        "recipe": [
-            ("oj", 120),
-            ("bacardi", 40)
-        ]
-    }, {
-        "name": "Bs",
-        "recipe": [
-            ("oj", 120),
-            ("bacardi", 40),
-            ("gren", 8)
-        ]
-    }, {
-            "name": "Ts 0 (1:6)",
-        "recipe": [
-            ("bacardi", 20),
-            ("mate", 120)
-        ]
-    }, {
-        "name": "Ts 1",
-        "recipe": [
-            ("bacardi", 40),
-            ("mate", 300)
-        ]
-    }, {
-        "name": "...under\nconstruction",
-        "recipe": [
-        ]
-    }
+ML = 1      # 1ml = 1g
+CL = 10     # 1cl = 10g
 
-]
+# syrup
+MLs = 1.4   # 1ml = 1.4g
+CLs = 14   # 1cl = 14g
 
 # "NAME":("NICENAME", ISWITHALCOHOL)
 ingredients = {
-    "gren": ("Grenadine", False),
-    "cocos": ("Cocos", False),
-    "lemonjuice": ("Lemon Juice", False),
-    "bacardi": ("Bacardi", True),
+    "vodka": ("Wodka", True),
     "rum": ("Rum", True),
-    "vodka": ("Vodka", True),
-    "gin": ("Gin", True),
-    "curacao": ("Blue Curacao", False),
-    "mango": ("Mango", False),
-    "pineapple": ("Pineapple", False),
-    "tequila": ("tequila", False),
-    "cola": ("Cola", False),
-    "oj": ("Orange Juice", False),
-    "mate": ("Mate", False),
+    "peach": ("Creme de Peche", True),
+    "cachaca": ("Cachaca", True),
+    "apricot": ("Arpicot Brandy"),
+
+    "porno": ("Porno", True),
+
+    "grenadine": ("Grenadinensirup", False),
+    "orange": ("Orangensaft", False),
+    "lemon": ("Zitronensaft", False),
+    "pineapple": ("Ananassaft", False),
+    "maracuja": ("Maracujasaft", False),
+    "soda": ("Soda", False),
+
 }
 
 
+drink_list = [
+    {
+        "name": "Touch Down Big",
+        "recipe": [
+            ("vodka", 6*CL),
+            ("apricot", 3*CL),
+            ("grenadine", 1.5*CLs),
+            ("lemon", 3*CL),
+            ("maracuja", 22.5*CL)
+        ]
+    },
+    {
+        "name": "Touch Down Light",
+        "recipe": [
+            ("vodka", 4 * CL),
+            ("apricot", 2 * CL),
+            ("grenadine", 1 * CLs),
+            ("lemon", 2 * CL),
+            ("maracuja", 15 * CL)
+        ]
+    },
+    {
+        "name": "Planter's Wonder",
+        "recipe": [
+            ("grenadine", 2*CLs),
+            ("lemon", 2*CL),
+            ("pineapple", 6*CL),
+            ("orange", 6*CL),
+            ("maracuja", 6*CL)
+        ]
+    }, {
+        "name": "Wodka Twist",
+        "recipe": [
+            ("vodka", 4*CL),
+            ("lemon", 2*CL),
+            ("grenadine", 1*CLs),
+        ]
+    }, {
+        "name": "Hawaii Cool",
+        "recipe": [
+            ("rum", 8*CL),
+            ("pineapple", 10*CL),
+            ("grenadine", 4*CLs),
+        ]
+    }, {
+        "name": "Lucky Driver",
+        "recipe": [
+            ("orange", 10*CL),
+            ("maracuja", 6*CL),
+            ("lemon", 2*CL),
+            ("grenadine", 1.5*CLs),
+        ]
+    }, {
+        "name": "The Beach",
+        "recipe": [
+            ("vodka", 4*CL),
+            ("peach", 3*CL),
+            ("lemon", 1*CLs),
+            ("orange", 4*CL),
+            ("grenadine", 1.5*CLs),
+            ("maracuja", 14*CL),
+        ]
+    }, {
+        "name": "The Beach",
+        "recipe": [
+            ("vodka", 4*CL),
+            ("peach", 3*CL),
+            ("lemon", 1*CL),
+            ("orange", 4*CL),
+            ("grenadine", 1.5*CLs),
+            ("maracuja", 14*CL),
+        ]
+    }, {
+        "name": "Peach Sling Light",
+        "recipe": [
+            ("vodka", 2*CL),
+            ("peach", 2*CL),
+            ("pineapple", 8*CL),
+            ("orange", 8*CL),
+        ]
+    }, {
+        "name": "Peach Sling Light",
+        "recipe": [
+            ("vodka", 2*CL),
+            ("peach", 2*CL),
+            ("pineapple", 8*CL),
+            ("orange", 8*CL),
+        ]
+    }, {
+        "name": "911 Targa",
+        "recipe": [
+            ("orange", 6*CL),
+            ("pineapple", 3*CL),
+            ("lemon", 3*CL),
+            ("grenadine", 3*CLs),
+            ("maracuja", 6*CL),
+        ]
+    }, {
+        "name": "Havana Sail",
+        "recipe": [
+            ("rum", 4*CL),
+            ("grenadine", 2*CLs),
+            ("maracuja", 8*CL),
+        ]
+    }, {
+        "name": "Nordic Summer",
+        "recipe": [
+            ("vodka", 4*CL),
+            ("maracuja", 4*CL),
+            ("lemon", 1*CL),
+            ("orange", 8*CL),
+            ("grenadine", 0.5*CLs),
+        ]
+    }, {
+        "name": "Grenadine Shake",
+        "recipe": [
+            ("lemon", 2*CL),
+            ("pineapple", 2*CL),
+            ("grenadine", 2*CLs),
+            ("soda", 4*CL),
+        ]
+    }, {
+        "name": "Tropical Sunshine",
+        "recipe": [
+            ("vodka", 2*CL),
+            ("maracuja", 6*CL),
+            ("orange", 6*CL),
+            ("grenadine", 0.2 * CLs),
+        ]
+    }, {
+        "name": "Tropical Sunshine",
+        "recipe": [
+            ("vodka", 4*CL),
+            ("maracuja", 12*CL),
+        ]
+    }, {
+        "name": "Wodka Sunrise",
+        "recipe": [
+            ("vodka", 4*CL),
+            ("orange", 12*CL),
+            ("lemon", 1*CL),
+            ("grenadine", 1*CLs)
+        ]
+    }, {
+        "name": "Waikiki",
+        "recipe": [
+            ("vodka", 4*CL),
+            ("pineapple", 12*CL),
+        ]
+    }, {
+        "name": "Finlady",
+        "recipe": [
+            ("vodka", 4*CL),
+            ("apricot", 2*CL),
+            ("lemon", 1*CL),
+        ]
+    }, {
+        "name": "Sixteen",
+        "recipe": [
+            ("grenadine", 2*CLs),
+            ("lemon", 2*CL),
+            ("pineapple", 4*CL),
+            ("orange", 8*CL),
+            ("maracuja", 2*CL),
+        ]
+    }, {
+        "name": "Bora Bora",
+        "recipe": [
+            ("grenadine", 1*CLs),
+            ("lemon", 1*CL),
+            ("pineapple", 10*CL),
+            ("maracuja", 6*CL),
+        ]
+    }, {
+        "name": "Ananasia",
+        "recipe": [
+            ("vodka", 2*CL),
+            ("peach", 3*CL),
+            ("pineapple", 2*CL),
+        ]
+    }, {
+        "name": "Golden Girl",
+        "recipe": [
+            ("vodka", 2*CL),
+            ("apricot", 3*CL),
+            ("orange", 6*CL),
+        ]
+    }, {
+        "name": "Screwdriver",
+        "recipe": [
+            ("vodka", 5*CL),
+            ("orange", 10*CL),
+        ]
+    }, {
+        "name": "Apricot Fizz",
+        "recipe": [
+            ("apricot", 4*CL),
+            ("lemon", 2*CL),
+            ("orange", 4*CL),
+            ("soda", 8*CL),
+        ]
+    }, {
+        "name": "Red One",
+        "recipe": [
+            ("grenadine", 1*CLs),
+            ("lemon", 1*CL),
+            ("pineapple", 5*CL),
+            ("orange", 5*CL),
+        ]
+    }, {
+        "name": "Los Angeles",
+        "recipe": [
+            ("vodka", 4*CL),
+            ("grenadine", 2*CLs),
+            ("lemon", 5*CL),
+            ("pineapple", 2*CL),
+            ("orange", 9*CL),
+            ("maracuja", 2*CL),
+        ]
+    }, {
+        "name": "Upton",
+        "recipe": [
+            ("rum", 2*CL),
+            ("lemon", 1*CL),
+            ("pineapple", 2*CL),
+            ("orange", 1*CL),
+        ]
+    }, {
+        "name": "National",
+        "recipe": [
+            ("rum", 3*CL),
+            ("apricot", 1*CL),
+            ("pineapple", 2*CL),
+        ]
+    }, {
+        "name": "Brasilian Sunrise",
+        "recipe": [
+            ("cachaca", 4*CL),
+            ("grenadine", 2*CLs),
+            ("lemon", 1*CL),
+            ("orange", 10*CL),
+        ]
+    }
+]
+
+
+def check_drinks():
+    keys = ingredients.keys()
+    for d in drink_list:
+        for r in d["recipe"]:
+            if r[0] not in keys:
+                print("ERROR in %s: %s" % (d["name"], r[0]))
